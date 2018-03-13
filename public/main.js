55ff898668986aa8be9e33a0d5a2b4d5ebd29bbc
Diboll <bdiboll@proforma.com>
2018-03-13 15:15:52 -0400

    basic js down

--- public/main.js
+++ public/main.js
@@ -1,5 +1,33 @@
+
 const main = () => {
-  document.querySelector('h1').textContent += '?'
+  
 }
 
 document.addEventListener('DOMContentLoaded', main)
+
+//Player 1
+
+const submitRock1 = () =>{
+  document.querySelector("#resultPlayer1").textContent = "Rock";
+}
+
+const submitPaper1 = () =>{
+  document.querySelector("#resultPlayer1").textContent = "Paper";
+}
+
+const submitScissors1 = () =>{
+  document.querySelector("#resultPlayer1").textContent = "Scissors";
+}
+
+//Player 2
+const submitRock2 = () =>{
+  document.querySelector("#resultPlayer2").textContent = "Rock";
+}
+
+const submitPaper2 = () =>{
+  document.querySelector("#resultPlayer2").textContent = "Paper";
+}
+
+const submitScissors2 = () =>{
+  document.querySelector("#resultPlayer2").textContent = "Scissors";
+}
